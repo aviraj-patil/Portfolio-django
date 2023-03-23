69eec6aaff6967ee7889134a58c804325b2ef6fd
aviraj <75115040+aviraj-patil@users.noreply.github.com>
2023-03-23 23:40:52 +0530

    Delete personal_portfolio-project directory

--- personal_portfolio-project/portfolio/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Project 
-
-
-def one(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio/onee.html', {'projects':projects})
